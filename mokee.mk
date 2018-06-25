08331437f0cedd3ccc789a02d7891b37c278c010
Martincz Gao <martincz@mokeedev.com>
2018-06-25 21:06:01 +0800

    hero2lte: mokee-ify
    Change-Id: Id7c25378335a445c671a199d4f7ed0a19f91360f

--- mokee.mk
+++ mokee.mk
@@ -24,11 +24,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := mk_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
